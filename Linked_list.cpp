8fbed46d9130df5bcc03c171f6073032e2066b0a
HitenR123 <pranjalkeshari2433@gmail.com>
2026-09-13 23:08:46 +0530

    update

--- Linked_list.cpp
+++ Linked_list.cpp
@@ -6,24 +6,24 @@ using namespace std;
 
 // create node
 
-struct Node{
-    int a;
-    Node* next;
-};
+// struct Node{
+//     int a;
+//     Node* next;
+// };
 
-int main(){
-    // Node* head = new Node{10,nullptr};
-    // head -> next = new Node{20,nullptr};
-    // head -> next -> next = new Node{30,nullptr};
+// int main(){
+//     // Node* head = new Node{10,nullptr};
+//     // head -> next = new Node{20,nullptr};
+//     // head -> next -> next = new Node{30,nullptr};
 
-    // Node* temp = head;
+//     // Node* temp = head;
 
-    // while(temp != nullptr){
-    //     cout<<temp->a<<" ";
-    //     temp=temp->next;
-    // }
+//     // while(temp != nullptr){
+//     //     cout<<temp->a<<" ";
+//     //     temp=temp->next;
+//     // }
 
-    // Node* head = new Node{};
-    // Node* temp = head;
-    // Node* newNode = new Node{};
-}
+//     // Node* head = new Node{};
+//     // Node* temp = head;
+//     // Node* newNode = new Node{};
+// }
